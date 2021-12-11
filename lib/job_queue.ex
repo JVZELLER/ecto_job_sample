1a89c49bf6cd5e549ca7c1dd9bf8d2d67887d015
José Victor Zeller <josevictorzg@gmail.com>
2021-12-11 11:56:47 -0300

    refactor: add some perform to test ecto job

--- lib/job_queue.ex
+++ lib/job_queue.ex
@@ -2,8 +2,43 @@ defmodule Sample.JobQueue do
   @moduledoc false
   use EctoJob.JobQueue, table_name: "jobs"
 
+  alias Ecto.Multi
+  alias Sample.Repo
+
+  require Logger
+
   @impl true
-  def perform(_multi, _params) do
-    {:ok, :ok}
+  def perform(multi, %{"type" => "normal_one"}) do
+    multi
+    |> Repo.transaction()
+    |> case do
+      _ -> {:ok, :nothign_to_do}
+    end
+  end
+
+  def perform(multi, %{"type" => "failed_one"}) do
+    multi
+    |> Multi.run(:update, fn _repo, _changes ->
+      {:error, :no_reason}
+    end)
+    |> Repo.transaction()
+    |> case do
+      {:error, step, _reason, _changes} = error ->
+        Logger.error("Fail to execute job due to errors in step '#{step}'")
+        error
+    end
+  end
+
+  def perform(multi, %{"type" => "buggy_one"}) do
+    multi
+    |> Multi.run(:insert_entry, fn _repo, _chnages ->
+      {:error, :already_exists}
+    end)
+    |> Repo.transaction()
+    |> case do
+      {:error, :insert_entry, :already_exists, _changes} ->
+        Logger.warn("Entry already exits, nothing to insert. Will successfully finish the job")
+        {:ok, :ok}
+    end
   end
 end
